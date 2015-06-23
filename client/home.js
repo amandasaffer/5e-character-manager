65d5881d0ece8fedb6e2431073cbcb77253af5bb
Amanda Saffer <amandasaffer@gmail.com>
2015-06-22 22:38:36 -0500

    set up template selection on hp for login / account creation

--- client/home.js
+++ client/home.js
@@ -1,3 +1,36 @@
 Template.home.rendered = function(e) {
 	$('.navbar').addClass('home-page');
+	Session.setDefault('homeStatus', 'default');
 };
+
+Template.home.helpers({
+	default: function() {
+		return Session.get('homeStatus') === 'default';
+	},
+
+	notMember: function() {
+		return Session.get('homeStatus') === 'notMember';
+	},
+
+	alreadyMember: function() {
+		return Session.get('homeStatus') === 'alreadyMember';
+	}
+});
+
+Template.intro.events({
+	'click .signup': function(e) {
+		// $('.container').fadeOut();
+		Session.set('homeStatus', 'notMember');
+	},
+
+	'click .login': function(e) {
+		// $('.container').fadeOut();
+		Session.set('homeStatus', 'alreadyMember');
+	}
+});
+
+Template.createAccount.events({
+	'click .go-back': function(e) {
+		Session.set('homeStatus', 'default');
+	},
+});
