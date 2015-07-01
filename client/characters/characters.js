9e9526a5dab5555acc6a75580f78b126ce53dc15
Amanda Saffer <amandasaffer@gmail.com>
2015-06-30 20:29:26 -0500

    update passive perception if proficiency modifier is changed

--- client/characters/characters.js
+++ client/characters/characters.js
@@ -32,7 +32,6 @@ applyProficiencyScores = function(abilityScoreModifier, proficiencyBonus, scoreI
 
 	// for unchecked elements, use base ability score modifier
 	abilityScoreModifier = addPositiveMod(abilityScoreModifier);
-
 	var matched = $('.saving-throws [data-score-index=' + scoreIndex + '], .general-proficiencies [data-score-index=' + scoreIndex + ']');
 	$(matched).text(abilityScoreModifier);
 
@@ -40,17 +39,17 @@ applyProficiencyScores = function(abilityScoreModifier, proficiencyBonus, scoreI
 	var combinedModifier = pureModifier + proficiencyBonus;
 	combinedModifier = addPositiveMod(combinedModifier);
 
-    $('input.gen-proficiency:checked, input.save-proficiency:checked').each(function() {
-    	// only apply modifier during loop if ability (str, dex, etc.) matches the proficiency
-    	if ($(this).parent().prev().data('score-index') === scoreIndex) {
-    		$(this).parent().prev().text(combinedModifier);
-    	}
-    });
+  $('input.gen-proficiency:checked, input.save-proficiency:checked').each(function() {
+  	// only apply modifier during loop if ability (str, dex, etc.) matches the proficiency
+  	if ($(this).parent().prev().data('score-index') === scoreIndex) {
+  		$(this).parent().prev().text(combinedModifier);
+  	}
+  });
 
-    if( $('.perception-prof').text().length > 0 ) {
-    	var addPerception = parseInt($('.perception-prof').text());
-    	$('input[name=passive-percep]').val(10 + addPerception);
-    }
+  if( $('.perception-prof').text().length > 0 ) {
+  	var addPerception = parseInt($('.perception-prof').text());
+  	$('input[name=passive-percep]').val(10 + addPerception);
+  }
 	return;
 };
 
@@ -70,13 +69,6 @@ Template.characters.helpers({
 Template.editCharacter.helpers({
 	class: function() {
 		return this.class;
-	},
-	// TODO: Get helper functions working
-	getAbilityScore: function(index) {
-		return this.abilityScores[index];
-	},
-	getAbilityModifier: function(index) {
-		return this.abilityModifiers[index];
 	}
 });
 
@@ -311,11 +303,10 @@ Template.editCharacter.events({
 		if( abilityScores.length < 6 || abilityScores[scoreIndex] != abilityScore ) {
 			applyProficiencyScores(modifier, proficiencyBonus, scoreIndex);
 		} else {
-			console.log('loop will run');
 			$('.base-modifier').each(function(scoreIndex, obj) {
-				var getMod = $(this).text();
-		     	applyProficiencyScores(getMod, proficiencyBonus, scoreIndex);
-		    });
+			var getMod = $(this).text();
+	     	applyProficiencyScores(getMod, proficiencyBonus, scoreIndex);
+	    });
 		}
 
 		// put ability score in array
@@ -332,50 +323,45 @@ Template.editCharacter.events({
 
 		// set ability score modifier
 		$(e.target).next().html(modifier);
+
 	},
 
-	// TODO: blurring this doesn't update passive perception if proficient. look into reactive-var.
 	'blur [name=proficiency]': function(e) {
 		e.preventDefault();
 		proficiencyBonus = $(e.target).val();
 
-		// if( perception .gen-proficiency isChecked ) {
-    // 	var addPerception = parseInt( $('[name=???]').text() );
-    // 	$('input[name=passive-percep]').val(10 + addPerception);
-    // }
-
-		// this is setting the data, not retrieving the value
-		if ( $('.gen-proficiency').data('add-proficiency-to', 'Perception').checked ) {
-			console.log('it\'s checked');
-		}
 
 		if ( abilityScores.length < 6 ) {
 			console.log('not all ability scores are filled out. do not apply proficiency bonuses yet.');
 		} else {
+			// update proficiency modifiers
 			$('.base-modifier').each(function(scoreIndex, obj) {
 				var getMod = $(this).text();
-		     	applyProficiencyScores(getMod, proficiencyBonus, scoreIndex);
-		    });
+	     	applyProficiencyScores(getMod, proficiencyBonus, scoreIndex);
+	    });
+
+			// update passive perception if it applies
+			for(i=0; i<proficiencies.length; i++) {
+				if(proficiencies[i] === 'Perception') {
+					var addPerception = parseInt($('[data-add-proficiency-to="Perception"]').parent().prev().text());
+					$('input[name=passive-percep]').val(10 + addPerception);
+					break;
+				}
+			}
 		}
 	},
 
 	'blur .weapon input': function(e) {
 		var weaponNumber = parseInt( $(e.target).closest('.weapon').data('weapon-number') );
 		var weaponIndex = parseInt( $(e.target).data('weapon-index') );
-
 		if(weapons.length - 1 < weaponNumber) {
 			weapons[weaponNumber] = [];
 		}
 		weapons[weaponNumber][weaponIndex] = $(e.target).val();
-
-		console.log(weapons);
 	},
 
 	'blur .trait input, blur .trait textarea': function(e) {
-		// get the trait number (first or second)
 		var traitNumber = traitCount - 1;
-
-		// get the value name and data associated
 		var traitIndex = parseInt( $(e.target).data('trait-index') );
 		var theData = $(e.target).val();
 
@@ -384,15 +370,11 @@ Template.editCharacter.events({
 			traits[traitNumber] = {};
 		}
 
-		// if blurring name
 		if (traitIndex === 0) {
 			traits[traitNumber].name = theData;
-		// if blurring description
 		} else {
 			traits[traitNumber].description = theData;
 		}
-
-		console.log(traits);
 	},
 
 	'click .save-proficiency, click .gen-proficiency': function(e) {
@@ -405,16 +387,12 @@ Template.editCharacter.events({
 		if( isChecked ) {
 			// checked after click
 			modifier = parseInt(modifier) + parseInt(proficiencyBonus);
-			// add to proficiencies array
 			proficiencies.push(thisProficiency);
 		} else {
 			modifier = parseInt(modifier) - parseInt(proficiencyBonus);
-			// remove from proficiencies array
 			var profIndex = proficiencies.indexOf(thisProficiency);
 			proficiencies.splice(profIndex, 1);
 		}
-
-		console.log(proficiencies);
 
 		if(modifier > 0) {
 			modifier = "+" + modifier;
@@ -425,7 +403,6 @@ Template.editCharacter.events({
 			$(e.target).parent().prev().text(modifier);
 		}
 
-		// if you're clicking perception, update passive perception score
 		// TODO: make this code block a function and re-use in create template
 		if( $(e.target).data('add-proficiency-to') === 'Perception') {
     	var addPerception = parseInt( $(e.target).parent().prev().text() );
@@ -435,7 +412,6 @@ Template.editCharacter.events({
 
 	'click .add-feat-trait': function(e) {
 		traitCount = traitCount + 1;
-		console.log(traitCount);
 		var newTrait = $("#charTrait").clone();
 
 		newTrait.children().find('input, textarea').each(function(){
@@ -447,48 +423,37 @@ Template.editCharacter.events({
 
 		$(newTrait).appendTo('#traits');
 
-		if(traitCount > 2) {
-			$('.add-feat-trait').css('margin-top', '50px');
-		} else {
-			$('.add-feat-trait').css('margin-top', '10px');
-		}
+		// TODO: determine if this is needed for the edit template
+		// if(traitCount > 3) {
+		// 	$('.add-feat-trait').css('margin-top', '50px');
+		// } else {
+		// 	$('.add-feat-trait').css('margin-top', '10px');
+		// }
 	},
 
 	'submit form': function(e) {
-
 		e.preventDefault();
-
 		var currentCharacterId = this._id;
 
 		var characterProperties = {
-	  		// overview
 			name: $(e.target).find('[name=charname]').val(),
 			class: $(e.target).find('[name=class]').val(),
 			level: $(e.target).find('[name=level]').val(),
 			background: $(e.target).find('[name=background]').val(),
 			race: $(e.target).find('[name=race]').val(),
 			alignment: $(e.target).find('[name=alignment]').val(),
-
-			// bullshit
 			ac: $(e.target).find('[name=ac]').val(),
 			initiative: $(e.target).find('[name=initiative]').val(),
 			speed: $(e.target).find('[name=speed]').val(),
 			hitPoints: $(e.target).find('[name=hitpoints]').val(),
 			hitDice: $(e.target).find('[name=hitdice]').val(),
-
-
-			// equipment
 			weapons: weapons,
 			equipment: $(e.target).find('[name=equipment]').val(),
-
-			// features and traits
 			traits: traits,
-
 			proficiency: proficiencyBonus,
 		 	passivePerception: $('input[name=passive-percep]').val(),
 			abilityScores: abilityScores,
 			abilityModifiers: abilityModifiers,
-
 			proficiencies: proficiencies
 		}
 
