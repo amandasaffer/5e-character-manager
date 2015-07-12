a27384810805b345ecdb175126be3f920597a78a
Amanda Saffer <amandasaffer@gmail.com>
2015-07-12 16:26:22 -0500

    broken form

--- client/characters/characters.js
+++ client/characters/characters.js
@@ -112,9 +112,6 @@ ReactiveForms.createElement({
   validationEvent: 'keyup'
 });
 
-
-
-
 Template.manageCharacter.events({
 	'blur .ability': function(e) {
 		e.preventDefault();
@@ -325,6 +322,29 @@ Template.characters.helpers({
 	characters: function() {
 		return Characters.find({userId: Meteor.user()._id, name: {"$exists": true }}, {sort: {timestamp : -1}});
 	}
+});
+
+
+
+
+Template['manageCharacterForm'].helpers({
+  schema: function () {
+    return CharacterSchema;
+  },
+  action: function () {
+    return function (els, callbacks, changed) {
+      console.log("[forms] Action running!");
+      console.log("[forms] Form data!", this);
+      console.log("[forms] HTML elements with `.reactive-element` class!", els);
+      console.log("[forms] Callbacks!", callbacks);
+      console.log("[forms] Changed fields!", changed);
+      callbacks.success(); // Display success message.
+      callbacks.reset();   // Run each Element's custom `reset` function to clear the form.
+    };
+  },
+	reset: function (el) {
+    $(el).val('');
+  }
 });
 
 // Template.displayCharacter.rendered = function(e) {
