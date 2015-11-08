2af4402d40f0a0fcf7f9731916bcf734e67b4c14
Amanda Saffer <asaffer@sosorgans.org>
2015-11-08 00:06:53 -0600

    remove horrendous form

--- client/characters/characters.js
+++ client/characters/characters.js
@@ -3,3 +3,9 @@ Template.characters.helpers({
 		return Characters.find({userId: Meteor.user()._id, name: {"$exists": true }}, {sort: {createdAt: -1}});
 	}
 });
+
+Template.characters.events({
+	'click #create': function(e) {
+		Router.go('insertCharacter');
+	}
+});
