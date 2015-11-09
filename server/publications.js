9dc5c3e32b3d8c1e3a49fdd97dc3b35c0294922e
Amanda Saffer <asaffer@sosorgans.org>
2015-11-08 21:53:31 -0600

    fix wonky user's characters publication

--- server/publications.js
+++ server/publications.js
@@ -8,11 +8,11 @@ Meteor.publish('characters', function() {
 	return Characters.find();
 });
 
-Meteor.publish('usersCharacters', function(user) {
-	return Characters.find({userId: user});
+Meteor.publish('usersCharacters', function() {
+	return Characters.find({userId: this.userId});
 });
 
 // TODO: publish a user's own groups
-Meteor.publish('groupsBelongedTo', function(user) {
-	return Characters.find({userId: user});
+Meteor.publish('groupsBelongedTo', function() {
+	return Characters.find({userId: this.userId});
 });
