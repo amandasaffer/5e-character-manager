5300463e88cd9bdd7e8dd24d1d0857763ccfd363
Amanda Saffer <asaffer@sosorgans.org>
2015-11-10 15:08:34 -0600

    ability score input markup, image upload markup, background / alignment info markup

--- client/characters/character_forms.js
+++ client/characters/character_forms.js
@@ -7,3 +7,16 @@
 Template.registerHelper("currentFieldValue", function (fieldName) {
   return AutoForm.getFieldValue(fieldName) || 'None';
 });
+
+Template.insertCharacter.helpers({
+	abilities: function () { 
+		return [
+			{ name: 'Strength', fieldName: 'abilityScores.0' },
+			{ name: 'Dexterity', fieldName: 'abilityScores.1' },
+			{ name: 'Constitution', fieldName: 'abilityScores.2' },
+			{ name: 'Intelligence', fieldName: 'abilityScores.3' },
+			{ name: 'Wisdom', fieldName: 'abilityScores.4' },
+			{ name: 'Charisma', fieldName: 'abilityScores.5' }
+		];
+	}
+});
